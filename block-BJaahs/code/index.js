f8eb49b8e0616b97b6dd794da71564c4e555c0e4
Sushil Ghate <sushilsg30@gmail.com>
2022-06-28 14:32:58 +0530

    block-BJaahs

--- block-BJaahs/code/index.js
+++ block-BJaahs/code/index.js
@@ -1,41 +1,41 @@
-let title = 'Where is the capital of Jordan';
-let options = ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'];
-let correctAnswerIndex = 1;
+// let title = 'Where is the capital of Jordan';
+// let options = ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'];
+// let correctAnswerIndex = 1;
 
-function isAnswerCorrect(index) {
-    return index === correctAnswerIndex;
-}
+// function isAnswerCorrect(index) {
+//     return index === correctAnswerIndex;
+// }
 
 
-function getCorrectAnswer() {
-    return options[correctAnswerIndex];
-}
+// function getCorrectAnswer() {
+//     return options[correctAnswerIndex];
+// }
 
-let question = {
-    title:'Where is the capital of Jordan',
-    options: ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
-    correctAnswerIndex: 1,
+// let question = {
+//     title:'Where is the capital of Jordan',
+//     options: ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
+//     correctAnswerIndex: 1,
 
-    isAnswerCorrect(index) {
-        return index === quetion.correctAnswerIndex;
-    },
-    getCorrectAnswer() {
-        return question.options[correctAnswerIndex];
-    },
-}; 
+//     isAnswerCorrect(index) {
+//         return index === quetion.correctAnswerIndex;
+//     },
+//     getCorrectAnswer() {
+//         return question.options[correctAnswerIndex];
+//     },
+// }; 
 
-let question2 = {
-    title:'Where is the capital of Jamaica',
-    options: ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
-    correctAnswerIndex: 1,
+// let question2 = {
+//     title:'Where is the capital of Jamaica',
+//     options: ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
+//     correctAnswerIndex: 1,
 
-    isAnswerCorrect(index) {
-        return index === quetion2.correctAnswerIndex;
-    },
-    getCorrectAnswer() {
-        return question2.options[correctAnswerIndex];
-    },
-}; 
+//     isAnswerCorrect(index) {
+//         return index === quetion2.correctAnswerIndex;
+//     },
+//     getCorrectAnswer() {
+//         return question2.options[correctAnswerIndex];
+//     },
+// }; 
 
 function createQuestion(title, options, correctAnswerIndex) {
     let question ={};
@@ -44,10 +44,10 @@ function createQuestion(title, options, correctAnswerIndex) {
     question.correctAnswerIndex = correctAnswerIndex;
 
     question.isAnswerCorrect = function(index) {
-        return index === question.correctAnswerIndex;
+        return index === this.correctAnswerIndex;
     };
     question.getCorrectAnswer = function(){
-        return question.options[question.correctAnswerIndex]
+        return this.options[this.correctAnswerIndex]
     };
     return question;
 }
