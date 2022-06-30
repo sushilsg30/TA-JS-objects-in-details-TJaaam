2c4e25611c4ecb0a261fcb78bf7480f5976d91b7
Sushil Ghate <sushilsg30@gmail.com>
2022-06-30 22:08:03 +0530

    block-BJaahw

--- block-BJaahw/code/index.js
+++ block-BJaahw/code/index.js
@@ -1,8 +1,18 @@
 // 1. Create a function `createUser` that accepts `name` and `age` and returns a new object with those properties.
+function createUser(name, age){
+    let newUser = {};
+    newUser.name = name;
+    newUser.age = age;
+} 
 
 // 2. Add a method named `sayHello` inside the object created above. When called it should alert `Welcome {user}`. Replace `{user}` with the name of the user.
 
+function sayHello(name) {
+    return `Welcome ${name}`;
+}
+
 // 3. Use the data (name, age) of two different person to create the object using `createUser`. Store the returned value in `personOne` and `personTwo`.
+
 
 // 4. Change the code inside `createUser` in such a way that the methods `sayHello` doesn't have to be in all object. Use the prototypal nature. (Hint Object.create())
 
