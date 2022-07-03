748a56cbf5e59e81418c7948cdfe6a451c14d0d0
Sushil Ghate <sushilsg30@gmail.com>
2022-07-03 10:46:39 +0530

    block-BJaaii

--- block-BJaaii/code/index.js
+++ block-BJaaii/code/index.js
@@ -1,48 +1,30 @@
-// 1. Create a class name `Stack` with the following data and methods. Also implement a `length` getter method.
-
-// Data:
-
-// - `stack`
-
-// Methods:
-
-// - `push`: will accept a value and add to the stack. Stack adds data at the end
-// - `pop`: will delete the last element (max index) of the stack
-// - `peek`: can accept and optional parameter. Will display the element at the given index(passed as argument). If nothing is passed display the last element (last index)
-// - `reverse`: will reverse all the elements of the stack and return the reversed stack
-// - `isEmpty`: returns `true` if the stack is empty and `false` if the stack has any data.
-// - `displayStack`: returns all the data in stack in string format
-
-// Getter
-
-// - `length`: returns the current length of the stack.
-
-
 class Stack{
-    constructor(stack){
-        this.stack = stack;
+    constructor(){
+        this.stack = [];
     }
-    push(value){
-        this.stack += value;
+    push(elm){
+        this.stack.push(elm);
+        return this.stack;
     }
     pop(){
-        this.stack - 1;
+        this.stack.pop();
+        return this.stack;
     }
-    peek(index = 1){
-
+    peek(index = this.stack.length - 1){
+        return this.stack[index];
     }
     reverse(){
         return this.stack.reverse();
     }
     isEmpty(){
-        return this.stack <= 1;
+        return !(this.stack.length > 0);
     }
     displayStack(){
-        return this.stack;
+        return this.stack.join(" ");
     }
 
-    get(){
-
+    get length(){
+        return this.stack.length;
     }
 
 }
@@ -61,3 +43,49 @@ console.log(myStack.peek()); // 'Two'
 console.log(myStack.isEmpty()); // false
 myStack.pop();
 console.log(myStack.isEmpty()); // true
+
+
+class Queue{
+    constructor(){
+        this.queue = [];
+    }
+    enqueue(item){
+        this.queue.push(item);
+        return this.queue;
+    }
+    dequeue(){
+        this.queue.splice(0, 1);
+        return this.queue;
+    }
+    peek(index = 0){
+        return this.queue[index];
+    }
+    isEmpty(){
+        return !(this.queue.length > 0);
+    }
+    displayQueue(){
+        return this.queue.join(" ");
+    }
+
+    get length(){
+        return this.queue.length;
+    }
+
+}
+
+
+let atmQueue = new Queue();
+atmQueue.enqueue('Aman');
+atmQueue.enqueue('John');
+atmQueue.enqueue('Rohan');
+console.log(atmQueue.displayQueue()); // "Aman John Rohan"
+console.log(atmQueue.length); // 3
+console.log(atmQueue.peek()); // "Aman"
+console.log(atmQueue.peek(1)); // "John"
+atmQueue.dequeue();
+console.log(atmQueue.length); // 2
+console.log(atmQueue.peek()); // 'John'
+console.log(atmQueue.isEmpty()); // false
+atmQueue.dequeue();
+atmQueue.dequeue();
+console.log(atmQueue.isEmpty()); // true
