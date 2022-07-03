7816c9d10bcb118fca289e95f97feb20314930d3
Sushil Ghate <sushilsg30@gmail.com>
2022-07-03 14:40:27 +0530

    block-BJaaij

--- block-BJaaij/code/index.js
+++ block-BJaaij/code/index.js
@@ -1,75 +1,53 @@
-// ## An object-oriented book-list!
-
-// - [ ] Create a class BookList
-// - [ ] Create another class called Book
-
-// #### Each Book should have several properties:
-
-// 1. Title
-// 2. Category
-// 3. Author
-// 4. isRead (default to `false`)
-// 5. finishedDate
-
-// Book class will have the following methods:
-
-// - `markBookAsRead` when called will change the `isRead` property on the book to be `true`. It will also change the `finishedDate` to be the `Date.now()` when this function is called.
-
-// #### BookList should have the following properties:
-
-// 1. An array of all the Books
-// 2. Current read book index
-
-// #### BookList Methods
-
-// - [] `add([Book])` will accept an array (list of books). Once the method is called the book will added to the list.
-// - [] `getCurrentBook()` should return the current book using the `index` and the book array
-// - [] `getNextBook()` should return the next book using the `index` and the book array
-// - [] `getPrevBook()` should return the previous book using the `index` and the book array
-// - [] `changeCurrentBook` should accept one parameter and update the current index.
-
-// After creating the Book and BookList class create 5 book object and add it to list. Test all the methods in Book and BookList class.
-
 class Book{
-    constructor(Title, Category, Author, isRead = false, finishedDate){
+    constructor(Title, Category, Author){
         this.Title = Title;
         this.Category = Category;
         this.Author = Author;
-        this.isRead = isRead;
-        this.finishedDate = finishedDate;
+        this.isRead = false;
+        this.finishedDate = null;
     }
     markBookAsRead(){
         this.isRead = true;
         this.finishedDate = Date.now();
     }
-
-
 }
 
-class BookList extends Book{
+class BookList {
     constructor(){
-        super(Title, Category, Author, isRead = false, finishedDate);
+        // super(Title, Category, Author, isRead = false, finishedDate);
         this.books = [];
-        this.currentBook = this.books[index = 0];
+        this.currentIndexBook = 0;
     }
-    add([book]){
-        this.books.push([book]);
+    add(books = []){
+        this.books = this.books.concat(books);
+        return this.books;
     }
-    getCurrentBook(index){
-        return this.books[index];
+    getCurrentBook(){
+        return this.books[this.currentIndexBook];
     }
-    getNextBook(index){
-        return this.books[index + 1];
+    getNextBook(){
+        this.currentIndexBook = this.currentIndexBook + 1;
+        return this.books[this.currentIndexBook];
     }
-    getPrevBook(index){
-        return this.books[index - 1];
+    getPrevBook(){
+        this.currentIndexBook = this.currentIndexBook - 1;
+        return this.books[this.currentIndexBook];
     }
-    changeCurrentBook(param){
-        this.currentBook = this.books[param];
+    changeCurrentBook(index){
+        this.currentIndexBook = index;
+        return this.currentIndexBook;
     }
 
 }
 
-let book1 = new Book("Sherlock Homes", "Fiction", "xyz", false, "24 Jan");
- 
+let book1 = new Book("Sherlock Homes", "Fiction", "Vijay");
+let book2 = new Book("Sherlock Homes2", "Fiction2", "Vijay2");
+let book3 = new Book("Sherlock Homes3", "Fiction3", "Vijay3");
+let book4 = new Book("Sherlock Homes4", "Fiction4", "Vijay4");
+let book5 = new Book("Sherlock Homes5", "Fiction5", "Vijay5");
+
+let library = new BookList();
+
+library.add([book1, book2, book3, book4, book5]);
+
 
